f530a358540baf7aa394b72100d108b5a5e76df3
stivi00 <stivisth@gmail.com>
2023-11-13 10:04:36 +0100

    added movie-card component

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.css'],
+})
+export class MovieCardComponent {}
