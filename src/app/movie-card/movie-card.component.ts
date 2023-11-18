6bddd37282e4b9309767c11eb4e58a4309820557
stivi00 <stivisth@gmail.com>
2023-11-18 09:36:50 +0100

    added documentation for movie-card component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,9 +1,20 @@
+/**
+ * @fileoverview
+ * @module app/movie-card
+ * @description This module contains the MovieCardComponent.
+ */
+
 import { Component, OnInit, Input } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MovieInfoComponent } from '../movie-info/movie-info.component';
 
+/**
+ * @classdesc
+ * MovieCardComponent represents a component for displaying movie information and managing favorites.
+ * @class
+ */
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
@@ -12,16 +23,27 @@ import { MovieInfoComponent } from '../movie-info/movie-info.component';
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
 
+  /**
+   * @constructor
+   * @param {FetchApiDataService} fetchApiData - Service for fetching movie data.
+   * @param {MatDialog} dialog - Service for displaying dialogs.
+   * @param {MatSnackBar} snackBar - Service for displaying snack bar notifications.
+   */
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
     public snackBar: MatSnackBar
   ) {}
 
+  //Lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
   ngOnInit(): void {
     this.getMovies();
   }
 
+  /**
+   * @method
+   * @description Fetches all movies using the FetchApiDataService.
+   */
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
@@ -30,7 +52,13 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
-  //open movie details
+  /**
+   * @method
+   * @description Gets information about movie.
+   * @param {string} name - Name of the movie.
+   * @param {string} description - Description of the movie.
+   *
+   */
   openMovieInfo(title: string, description: string): void {
     this.dialog.open(MovieInfoComponent, {
       data: {
@@ -40,7 +68,13 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
-  // open genre details
+  /**
+   * @method
+   * @description Gets information about genre.
+   * @param {string} name - Name of the genre
+   * @param {string} description - Description of the genre.
+   * .
+   */
   getGenre(name: string, description: string): void {
     this.dialog.open(MovieInfoComponent, {
       data: {
@@ -50,7 +84,13 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
-  // open director details
+  /**
+   * @method
+   * @description Gets information about director.
+   * @param {string} name - Name of the director.
+   * @param {string} bio - Short biography of the director.
+   *
+   */
   getDirector(name: string, bio: string): void {
     this.dialog.open(MovieInfoComponent, {
       data: {
@@ -60,7 +100,11 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
-  // add movie favorites
+  /**
+   * @method
+   * @description Adds a movie to favorites.
+   * @param {string} id - The ID of the movie to be added to favorites.
+   */
   addFavorite(id: string): void {
     this.fetchApiData.addFavoriteMovie(id).subscribe(() => {
       this.snackBar.open('Movie added to favorites', 'OK', {
@@ -74,7 +118,11 @@ export class MovieCardComponent implements OnInit {
     return this.fetchApiData.isFavorite(id);
   }
 
-  // remove movie from favorites
+  /**
+   * @method
+   * @description Removes a movie from favorites.
+   * @param {string} id - The ID of the movie to be removed from favorites.
+   */
   removeFavorite(id: string): void {
     this.fetchApiData.deleteFavoriteMovie(id).subscribe(() => {
       this.snackBar.open('Movie is removed from favorites', 'OK', {
