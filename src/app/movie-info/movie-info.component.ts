37a9805d7d9ab4def9224d532fad29ba2d7ca491
stivi00 <stivisth@gmail.com>
2023-11-18 09:39:46 +0100

    documentation for movie-info component added

--- src/app/movie-info/movie-info.component.ts
+++ src/app/movie-info/movie-info.component.ts
@@ -1,12 +1,29 @@
+/**
+ * @fileoverview
+ * @module app/movie-info
+ * @description This module contains the MovieInfoComponent.
+ */
+
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+/**
+ * @classdesc
+ * MovieInfoComponent represents a dialog component for displaying movie information.
+ * @class
+ */
 @Component({
   selector: 'app-movie-info',
   templateUrl: './movie-info.component.html',
   styleUrls: ['./movie-info.component.css'],
 })
 export class MovieInfoComponent implements OnInit {
+  /**
+   * @constructor
+   * @param {Object} data - The data object injected into the component.
+   * @param {string} data.Title - The title of the movie.
+   * @param {string} data.Description - The description or details of the movie.
+   */
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
@@ -14,6 +31,6 @@ export class MovieInfoComponent implements OnInit {
       Description: string;
     }
   ) {}
-
+  // Lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
   ngOnInit(): void {}
 }
